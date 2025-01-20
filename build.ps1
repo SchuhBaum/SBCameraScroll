884828a5f13de237fbea9d2de05d502f83fd3abe
schuhbaum <schuhbaum71@gmail.com>
2025-01-20 16:29:12 +0100

    -

--- build.ps1
+++ build.ps1
@@ -1,7 +1,4 @@
 
-$wd = Get-Location
-Set-Location "c:\steam\steamapps\common\rain world\modding\sbcamerascroll\sourcecode"
-
 $config = "Release"
 dotnet build -c $config
 
@@ -16,6 +13,4 @@ Move-Item -Path $sourcePath -Destination $destinationPath -Force
 $sourcePath      = Get-ChildItem -Path ("bin\" + $config) -Recurse -Filter $pdbName | Select-Object -ExpandProperty FullName
 $destinationPath = Join-Path -Path ("..\" + $modName + "\plugins\") -ChildPath $pdbName
 Move-Item -Path $sourcePath -Destination $destinationPath -Force
-
-Set-Location $wd
 
---
+++ build_linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd "./SourceCode" || exit
+
+config="Release"
+dotnet build -c "$config"
+
+mod_name="SBCameraScroll"
+dll_name="$mod_name.dll"
+pdb_name="$mod_name.pdb"
+
+dest_path="../$mod_name/plugins"
+mkdir -p $dest_path
+
+src_file=$(find "bin/$config" -type f -name "$dll_name")
+dest_file="$dest_path/$dll_name"
+mv -f "$src_file" "$dest_file"
+
+src_file=$(find "bin/$config" -type f -name "$pdb_name")
+dest_file="$dest_path/$pdb_name"
+mv -f "$src_file" "$dest_file"
+
+cd ..
